5aa17df10e7e1ac207dd64413084ccd68e582a19
lukeyouell <>
2018-05-10 16:15:09 +0100

    Templates

---
+++ src/assetbundles/TicketBundle.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Support plugin for Craft CMS 3.x
+ *
+ * Simple support system for tracking, prioritising and solving customer support tickets.
+ *
+ * @link      https://github.com/lukeyouell
+ * @copyright Copyright (c) 2018 Luke Youell
+ */
+
+namespace lukeyouell\support\assetbundles;
+
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+class TicketBundle extends AssetBundle
+{
+    public function init()
+    {
+        // define the path that your publishable resources live
+        $this->sourcePath = '@lukeyouell/support/assetbundles';
+
+        // define the dependencies
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        // define the relative path to CSS/JS files that should be registered with the page
+        // when this asset bundle is registered
+        //$this->js = [
+        //    'script.js',
+        //];
+
+        $this->css = [
+            'css/ticket.css',
+        ];
+
+        parent::init();
+    }
+}
--- src/elements/db/TicketQuery.php
+++ src/elements/db/TicketQuery.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Support plugin for Craft CMS 3.x
+ *
+ * Simple support system for tracking, prioritising and solving customer support tickets.
+ *
+ * @link      https://github.com/lukeyouell
+ * @copyright Copyright (c) 2018 Luke Youell
+ */
 
 namespace lukeyouell\support\elements\db;
 
