0354849687820ac26430030b282d3ecb247446b2
lukeyouell <>
2018-05-23 09:08:35 +0100

    Corrected version number
    Not ready for release

--- src/Support.php
+++ src/Support.php
@@ -30,24 +30,6 @@ use craft\web\twig\variables\CraftVariable;
 
 use yii\base\Event;
 
-/**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://craftcms.com/docs/plugins/introduction
- *
- * @author    Luke Youell
- * @package   Support
- * @since     1.0.0
- *
- * @property  SupportServiceService $supportService
- * @property  Settings $settings
- * @method    Settings getSettings()
- */
 class Support extends Plugin
 {
     // Static Properties
@@ -69,22 +51,11 @@ class Support extends Plugin
      *
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public $schemaVersion = '0.1.0';
 
     // Public Methods
     // =========================================================================
 
-    /**
-     * Set our $plugin static property to this class so that it can be accessed via
-     * Support::$plugin
-     *
-     * Called after the plugin class is instantiated; do any one-time initialization
-     * here such as hooks and events.
-     *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
-     *
-     */
     public function init()
     {
         parent::init();
